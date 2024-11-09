c39924e2df6467c5717ec460edc5db5bd44157ee
Kavindu <kavindu2308@gmail.com>
2024-11-09 14:03:37 +0530

    updated the server code

--- api/index.js
+++ api/index.js
@@ -2,26 +2,26 @@ require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
 const { google } = require('googleapis');
+const cors = require('cors');
+
+// Decode the base64-encoded JSON content
+const credentials = JSON.parse(Buffer.from(process.env.GOOGLE_SERVICE_ACCOUNT_JSON, 'base64').toString('utf-8'));
+
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-const cors = require('cors');
+// Middleware and route setup
+app.use(express.json());
 app.use(cors({
-  origin: 'http://localhost:3000', // Replace with your frontend's URL
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000', // CORS with frontend URL
 }));
-
-const credentials = require('./config/google-service-account.json'); 
-
-app.use(bodyParser.json());
 
 // Google Sheets setup
 const auth = new google.auth.GoogleAuth({
   credentials,
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
-
 const sheets = google.sheets({ version: 'v4', auth });
-
 
 const SPREADSHEET_ID = process.env.SHEET_ID;
 
